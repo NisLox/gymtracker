ebfab3f8a0f94b5bae97a2731468f0d542c24e74
Will <Willburgess95@gmail.com>
2019-06-03 19:28:41 +0100

    better table

--- GymTracker/src/app-Component/App.js
+++ GymTracker/src/app-Component/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './header'
 import Container from './container-Component/container';
+import 'typeface-roboto';
 
 function App() {
   return (
--- GymTracker/src/app-Component/container-Component/exerciseTable-Component/exerciseTable.js
+++ GymTracker/src/app-Component/container-Component/exerciseTable-Component/exerciseTable.js
@@ -1,57 +1,74 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 import AddExerciseButton from './addExerciseButton'
 import InputResultsButton from './inputResultsButton'
-import Table from 'react-bootstrap/Table'
 
-class ExerciseTable extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {  };
-    }
-    hello() {
-        console.log("AddExerciseButton")
-    }
-    render() {
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 650,
+  },
+}));
 
-        
-        return (
-            <div>
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                        <th>Exercise</th>
-                        <th>Set 1</th>
-                        <th>Set 2</th>
-                        <th>Set 3</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                        <td>Tricep pull downs</td>
-                        <td>10x20</td>
-                        <td>30x20</td>
-                        <td>40x20</td>
-                        </tr>
-                        <tr>
-                        <td>Bench Press</td>
-                        <td>60x10</td>
-                        <td>70x10</td>
-                        <td>80x10</td>
-                        </tr>
-                        <tr>
-                        <td>Sit ups</td>
-                        <td>50</td>
-                        <td>30</td>
-                        <td>20</td>
-                        </tr>
-                    </tbody>
-                </Table>
-                <AddExerciseButton name="Add Exercise.." handleClick={this.hello}/>
-                <InputResultsButton name="Input Results.." handleClick={this.hello} />
-            </div>
-           
-        )
-    }
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
 }
 
-export default ExerciseTable;
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+function SimpleTable() {
+  const classes = useStyles();
+
+  return (
+      <div>
+        <Paper className={classes.root}>
+        <Table className={classes.table}>
+            <TableHead>
+            <TableRow>
+                <TableCell>Dessert (100g serving)</TableCell>
+                <TableCell align="right">Calories</TableCell>
+                <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            </TableRow>
+            </TableHead>
+            <TableBody>
+            {rows.map(row => (
+                <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                    {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+                </TableRow>
+            ))}
+            </TableBody>
+        </Table>
+        </Paper>
+
+        <AddExerciseButton name={"Add Exercise..."} />
+        <InputResultsButton name={"Input Results..."} />
+    </div>
+  );
+  
+}
+
+export default SimpleTable;
