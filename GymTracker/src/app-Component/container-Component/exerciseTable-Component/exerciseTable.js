1c209f8526141dd3258751f6ab6bbcda7300e2a1
Will <Willburgess95@gmail.com>
2019-06-03 19:02:42 +0100

    table with demo material

--- GymTracker/src/app-Component/container-Component/exerciseTable-Component/exerciseTable.js
+++ GymTracker/src/app-Component/container-Component/exerciseTable-Component/exerciseTable.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AddExerciseButton from './addExerciseButton'
 import InputResultsButton from './inputResultsButton'
+import Table from 'react-bootstrap/Table'
 
 class ExerciseTable extends React.Component {
     constructor(props) {
@@ -15,7 +16,36 @@ class ExerciseTable extends React.Component {
         
         return (
             <div>
-                <h1>ExerciseTable.js</h1>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                        <th>Exercise</th>
+                        <th>Set 1</th>
+                        <th>Set 2</th>
+                        <th>Set 3</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td>Tricep pull downs</td>
+                        <td>10x20</td>
+                        <td>30x20</td>
+                        <td>40x20</td>
+                        </tr>
+                        <tr>
+                        <td>Bench Press</td>
+                        <td>60x10</td>
+                        <td>70x10</td>
+                        <td>80x10</td>
+                        </tr>
+                        <tr>
+                        <td>Sit ups</td>
+                        <td>50</td>
+                        <td>30</td>
+                        <td>20</td>
+                        </tr>
+                    </tbody>
+                </Table>
                 <AddExerciseButton name="Add Exercise.." handleClick={this.hello}/>
                 <InputResultsButton name="Input Results.." handleClick={this.hello} />
             </div>
